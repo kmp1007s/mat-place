cbda9e3191c0581942059d997ebca5e9eb97a084
김세한 <kmp1007s@naver.com>
2020-03-30 23:56:17 +0900

    Init Project
    Init Project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import KakaoMapDemo from "./KakaoMapDemo";
+
+function App() {
+  return <div className="App">{/* <KakaoMapDemo /> */}</div>;
+}
+
+export default App;
---
+++ frontend/src/KakaoMapDemo.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+interface Props {}
+
+const KakaoMapDemo = (props: Props) => {
+  useEffect(() => {
+    let container = document.getElementById("map");
+    let options = {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3
+    };
+
+    let map = new window.kakao.maps.Map(container, options);
+  }, []);
+
+  return <div id="map" style={{ width: "100vw", height: "100vh" }} />;
+};
+
+export default KakaoMapDemo;
