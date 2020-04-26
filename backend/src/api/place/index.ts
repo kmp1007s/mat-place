885dab113f80da202c9edc402e9b12e2ba5d3b04
SaeHan Kim <kmp1007s@naver.com>
2020-04-26 18:36:15 +0900

    Connect Controller To Router
    Connect Controller To Router

--- backend/src/api/place/index.ts
+++ backend/src/api/place/index.ts
@@ -7,6 +7,7 @@ place.use(loginMiddleWare);
 
 place.get("/", placeCtrl.readPlaceList);
 place.post("/", placeCtrl.cretePlaceList);
+place.post("/groups", placeCtrl.createGroup);
 place.patch("/groups/:name", placeCtrl.updateGroup);
 place.delete("/groups/:name", placeCtrl.deleteGroup);
 
