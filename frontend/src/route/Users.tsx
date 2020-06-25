b7180d5627dd231697b061fc766f5ff62538aad7
SaeHan Kim <kmp1007s@naver.com>
2020-06-25 10:36:55 +0900

    Add sub routes
    Add sub routes

--- frontend/src/route/Users.tsx
+++ frontend/src/route/Users.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import { RouteComponentProps, Route, Switch } from "react-router-dom";
 
 import UserDetail from "page/User/UserDetail";
+import NotFoundPage from "page/NotFound";
 
 function Users({ match }: RouteComponentProps) {
   return (
     <Switch>
       <Route exact path={`${match.path}/:id`} component={UserDetail} />
+      <Route component={NotFoundPage} />
     </Switch>
   );
 }
