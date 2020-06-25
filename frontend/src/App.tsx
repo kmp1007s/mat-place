299c017c5fee67e76204f130078adeb770f87bc6
SaeHan Kim <kmp1007s@naver.com>
2020-06-25 10:37:12 +0900

    Update app
    Update app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Route from "./route";
+import Cookies from "js-cookie";
+import { useDispatch } from "react-redux";
+import { tokenCheck } from "modules/login";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log(Cookies.get("token"));
+    dispatch(tokenCheck());
+  }, [dispatch]);
+
   return <Route />;
 }
 
